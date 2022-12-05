0c328b2b5247916621ee014bdc8f4035f0033209
spartans-8327 <spartanscbtis203@gmail.com>
2022-12-05 16:58:00 -0600

    empece con las funciones para movers por cuadros, no he probado nada, probar lo antes posible para continuar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/domain/Posicion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/domain/Posicion.java
@@ -3,50 +3,49 @@ package org.firstinspires.ftc.teamcode.domain;
 public class Posicion {
 
     Chasis chasis;
-    private double posicionActual [];
+    private double posicionActualCuadros[];
+    private double posicionActualPulsos[];
 
     private int pulsos_X = 0;
     private int pulsos_Y = 0;
 
     public Posicion(Chasis chasis, double posicionX , double posicionY){
         this.chasis = chasis;
-        this.posicionActual[0] = posicionX;
-        this.posicionActual[1] = posicionY;
-    }
-
-    public void moverseCuadroPosicion(double metaX, double metaY, double potencia){
-        
+        this.posicionActualCuadros[0] = posicionX;
+        this.posicionActualCuadros[1] = posicionY;
     }
 
     public void moverseNcuadrosX(int numeroCuadrosX, double potencia){
         chasis.moverseX(potencia, pulsos_X * numeroCuadrosX);
+        this.posicionActualCuadros[0] += numeroCuadrosX;
     }
 
     public void moverseNcuadrosY(int numeroCuadrosY, double potencia){
         chasis.moverseY(potencia, pulsos_X * numeroCuadrosY);
+        this.posicionActualCuadros[1] += numeroCuadrosY;
     }
 
 
-    private void moverseCuadroX(double potencia){
+    private void moverse1CuadroX(double potencia){
         chasis.moverseX(potencia, pulsos_X);
     }
-    private void moverseCuadroY(double potencia){
+    private void moverse1CuadroY(double potencia){
         chasis.moverseY(potencia, pulsos_Y);
     }
 
     private double calcularErrorX(double metaX){
-        double ErrorX = posicionActual[0] - metaX;
+        double ErrorX = posicionActualCuadros[0] - metaX;
         return ErrorX;
     }
 
     private double calcularErrorY(double metaY){
-        double ErrorY = posicionActual[1] - metaY;
+        double ErrorY = posicionActualCuadros[1] - metaY;
         return  ErrorY;
     }
 
     public void actualizarPosicion(double posicionX , double posicionY){
-        this.posicionActual[0] = posicionX;
-        this.posicionActual[1] = posicionY;
+        this.posicionActualCuadros[0] = posicionX;
+        this.posicionActualCuadros[1] = posicionY;
     }
 
     }
